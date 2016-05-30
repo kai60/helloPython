8e4a93d75e04845b059ed30b64c9a4c5ca10f7f0
kai <zhufadong@163.com>
2016-05-30 18:06:44 +0800

    骨架
    创建骨架

---
+++ app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: Kai
+@license: Apache Licence 
+@contact: zhufadong@163.com
+@site: 
+@software: PyCharm
+@file: app.py
+@time: 16/5/30 17:08
+"""
+
+import logging;logging.basicConfig(level=logging.INFO)
+import aiohttp,os,json,time,asyncio
+from datetime import datetime
+from aiohttp import web
+def index(request):
+    return web.Response(body='<h1>hellp,python</h1>'.encode(encoding='utf-8'),content_type='text/html',charset='uft-8')
+
+@asyncio.coroutine
+def init(loop):
+    app=web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    sever=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at 127.0.0.1')
+    return sever
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    loop=asyncio.get_event_loop()
+    loop.run_until_complete(init(loop))
+    loop.run_forever()
